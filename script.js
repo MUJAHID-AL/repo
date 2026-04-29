826a96549188a2ec00232c317b1a12e2d7a0cabb
mujahid <mmcollection023@gmail.com>
2026-04-29 18:59:21 +0500

    add

---
+++ script.js
@@ -0,0 +1,12 @@
+// array
+// let numbers = [20,30,40];
+// object's
+// let student = {
+// id: "702209",
+// name:"mujahid",
+// course:"web development"
+// } ;
+// console.log(student);
+// functions
+
+
